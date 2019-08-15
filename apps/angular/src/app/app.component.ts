f84f95cd50344b2e051b620daef4b387559453a2
Derek Batenhorst <derek.batenhorst@buildertrend.com>
2019-08-15 12:52:13 -0500

    Added react components

--- apps/angular/src/app/app.component.ts
+++ apps/angular/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'modern-concepts-root',
+  selector: 'mc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
