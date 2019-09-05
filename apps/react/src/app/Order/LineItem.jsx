4492324569261c2728b750aa4ac54c370adb4b02
Derek Batenhorst <derek.batenhorst@buildertrend.com>
2019-09-04 21:20:14 -0500

    Updated line items components with multi child panel

--- apps/react/src/app/Order/LineItem.jsx
+++ apps/react/src/app/Order/LineItem.jsx
@@ -1,20 +1,24 @@
 // LineItems.jsx
 import React from 'react';
+import PanelMultipleChildren from '../composition/Panel/PanelMultipleChildren';
 
 export function LineItem(props) {
-    const handleEditClick = React.useCallback(() => {
-        // LineItem.jsx
-        props.onEditClick(props.item.id);
-    }, [props.item.id]);
-    return (
-        <>
-            <div>Name: {props.item.name}</div>
-            <div>Description: {props.item.description}</div>
-            <div>Price: ${props.item.price}</div>
-            <div>
-                <button onClick={handleEditClick}>Edit</button>
-            </div>
-        </>
-    );
+  const handleEditClick = React.useCallback(() => {
+    props.onEditClick(props.item.id);
+  }, [props.item.id]);
+
+  // LineItem.jsx
+  return (
+    <PanelMultipleChildren
+      title={<h3>{props.item.name}</h3>}
+      body={<>
+        <div>Description: {props.item.description}</div>
+        <div>Price: ${props.item.price}</div>
+        <div>
+          <button onClick={handleEditClick}>Edit</button>
+        </div>
+      </>}
+    />
+  );
 }
 
